0f4faf5d97ab6f5772b1be92637c4186ebcb80bc
CheriseCodes <cherise.bry@gmail.com>
2023-12-06 20:15:01 -0500

    no more separate update script

--- aws/update_s3.sh
+++
@@ -1,3 +0,0 @@
-aws cloudformation update-stack --stack-name flash-card-app-s3 --template-body file://templates/s3.yaml \
-    --parameters ParameterKey=BucketName,ParameterValue=$BUCKET_NAME \
-    ParameterKey=DistributionId,ParameterValue=$DISTRIBUTION_ID

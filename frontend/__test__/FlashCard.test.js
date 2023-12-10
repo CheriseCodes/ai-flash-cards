5c90969300120b4c7a2473c1b43de168af2dff96
CheriseCodes <cherise.bry@gmail.com>
2023-12-10 12:24:06 -0500

    use mock service worker

--- frontend/__test__/FlashCard.test.js
+++ frontend/__test__/FlashCard.test.js
@@ -5,8 +5,6 @@
 import FlashCard from '../src/components/FlashCard.js';
 import { renderWithProviders } from './test-utils.js';
 
-// TODO: start using https://mswjs.io/docs/getting-started
-
 it("screen height greater than zero", () => {
     renderWithProviders(<FlashCard cardData={{id:"abcd123"}} userId="default"></FlashCard>);
     expect(screen.height > 0);

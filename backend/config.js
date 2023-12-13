5033a9412c31c9642de82377e98dd13b521ebb79
CheriseCodes <cherise.bry@gmail.com>
2023-12-13 16:50:15 -0500

    export allowed words

--- backend/config.js
+++ backend/config.js
@@ -1,27 +1,3 @@
-const appConfig = {
-  languageModes: {
-    KOREAN: "Korean",
-    FRENCH: "French",
-    SPANISH: "Spanish",
-  },
-  cferLanguageLevels: {
-    A1: "A1",
-    A2: "A2",
-    B1: "B1",
-    B2: "B2",
-    C1: "C1",
-    C2: "C2",
-  },
-  koreanLanguageLevels: {
-    TOPIK1: "TOPIK1",
-    TOPIK2: "TOPIK2",
-    TOPIK3: "TOPIK3",
-    TOPIK4: "TOPIK4",
-    TOPIK5: "TOPIK5",
-    TOPIK6: "TOPIK6",
-  },
-};
-
 const allowedFrenchWords = [
   "aller",
   "avoir",
@@ -335,5 +311,32 @@ const allowedKoreanWords = [
   "일찍",
   "아마",
 ];
+
+const appConfig = {
+  languageModes: {
+    KOREAN: "Korean",
+    FRENCH: "French",
+    SPANISH: "Spanish",
+  },
+  cferLanguageLevels: {
+    A1: "A1",
+    A2: "A2",
+    B1: "B1",
+    B2: "B2",
+    C1: "C1",
+    C2: "C2",
+  },
+  koreanLanguageLevels: {
+    TOPIK1: "TOPIK1",
+    TOPIK2: "TOPIK2",
+    TOPIK3: "TOPIK3",
+    TOPIK4: "TOPIK4",
+    TOPIK5: "TOPIK5",
+    TOPIK6: "TOPIK6",
+  },
+  allowedFrenchWords: allowedFrenchWords,
+  allowedSpanishWords: allowedSpanishWords,
+  allowedKoreanWords: allowedKoreanWords
+};
 
 export default appConfig;

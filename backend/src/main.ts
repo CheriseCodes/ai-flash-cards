ce280cb31ad56dff081d45b51ef2c4b16d243b7f
CheriseCodes <cherise.bry@gmail.com>
2023-12-14 21:34:31 -0500

    close on sigint or sigterm

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,19 @@
 import { app } from "./server";
 const PORT = 8000;
-app.listen(PORT, () => console.log("server is running on port " + PORT));
+const server = app.listen(PORT, () => console.log("server is running on port " + PORT));
+process.on('SIGTERM', () => {
+    console.info('SIGTERM signal received.');
+    console.log('Closing http server.');
+    server.close((err) => {
+      console.log('Http server closed.');
+      process.exit(err ? 1 : 0);
+    });
+  });
+  process.on('SIGINT', () => {
+    console.info('SIGINT signal received.');
+    console.log('Closing http server.');
+    server.close((err) => {
+      console.log('Http server closed.');
+      process.exit(err ? 1 : 0);
+    });
+  });

be2e4b6ed99e3fd5587e5774770b6fa3c5c950ab
CheriseCodes <cherise.bry@gmail.com>
2023-12-20 21:57:19 -0500

    test generating text

--- frontend/__test__/FlashCard.test.js
+++ frontend/__test__/FlashCard.test.js
@@ -4,7 +4,7 @@
 import React from 'react';
 import FlashCard from '../src/components/FlashCard.js';
 import { renderWithProviders } from './test-utils.js';
-// import { screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 
 it("should have the correct values for a valid flashcard", () => {
     const id = "abc123";
@@ -22,8 +22,46 @@ it("should have the correct values for a valid flashcard", () => {
     const { container } = renderWithProviders(<FlashCard key={id} cardData={cardData} setErrors={setErrors} userId="default"></FlashCard>);
     const sentences = container.querySelectorAll("textarea.flash-card-sentence");
     const words = container.querySelectorAll("input.flash-card-word");
+    const img = container.querySelector("img");
     expect(sentences[0].innerHTML).toBe("Bonjour tout le monde!");
     expect(sentences[1].innerHTML).toBe("Hello everyone!");
     expect(words[0].value).toBe("bonjour");
     expect(words[1].value).toBe("hello");
+    expect(img.src).toBe("https://picsum.photos/256");
+});
+it("should display loading spinner when image is being generated", () => {
+    const id = "abc123";
+    const setErrors = (x) => {return [x]; };
+    const cardData = {
+                        id: id, 
+                        generatingText: false,
+                        generatingImage: true,
+                        word: "bonjour",
+                        wordTranslated: "hello",
+                        sampleSentence: "Bonjour tout le monde!",
+                        translatedSampleSentence: "Hello everyone!",
+                        img: null
+                    }
+    const { container } = renderWithProviders(<FlashCard key={id} cardData={cardData} setErrors={setErrors} userId="default"></FlashCard>);
+    const imgContainer = container.querySelector(".image-container");
+    expect(imgContainer.children.length).toBe(1);
+    expect(imgContainer.children[0].className).toBe("loading-spinner");
+});
+it("should display loading spinner when text is being generated", () => {
+    const id = "abc123";
+    const setErrors = (x) => {return [x]; };
+    const cardData = {
+                        id: id, 
+                        generatingText: true,
+                        generatingImage: false,
+                        word: "bonjour",
+                        wordTranslated: null,
+                        sampleSentence: null,
+                        translatedSampleSentence: null,
+                        img: null
+                    }
+    const { container } = renderWithProviders(<FlashCard key={id} cardData={cardData} setErrors={setErrors} userId="default"></FlashCard>);
+    const imgContainer = container.querySelector(".flash-card");
+    expect(imgContainer.children.length).toBe(1);
+    expect(imgContainer.children[0].className).toBe("loading-spinner");
 });
--- frontend/src/components/FlashCard.js
+++ frontend/src/components/FlashCard.js
@@ -143,7 +143,7 @@ const FlashCard = ({ cardData, setErrors, userId }) => {
               ></textarea>
               <div className="image-container">
                 {!cardData.generatingImage ? (
-                  <img height={250} width={250} src={cardData.img} crossOrigin="anonymous" onError={process.env.NODE_ENV == "development" ? () => {} : reloadImage} onLoad={loadedImage} hidden></img>
+                  <img height={250} width={250} src={cardData.img} crossOrigin="anonymous" alt={`Visualization of "${cardData.translatedSampleSentence}"`} onError={process.env.NODE_ENV == "development" ? () => {} : reloadImage} onLoad={loadedImage} hidden></img>
                 ) : (
                   <LoadingSpinner />
                 )}

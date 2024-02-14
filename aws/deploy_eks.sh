38ca407ae8956ea23335b76c6f0471a26a8d8717
CheriseCodes <cherise.bry@gmail.com>
2024-02-13 20:32:19 -0500

    start eks configuration

--- aws/deploy_eks.sh
+++ aws/deploy_eks.sh
@@ -1,9 +1,20 @@
-eksctl create cluster --name ai-flash-cards-cluster --region ca-central-1 --fargate
-eksctl create iamidentitymapping \
-    --cluster ai-flash-cards-cluster \
-    --region ca-central-1 \
-    --arn $CLI_ROLE_ARN \
-    --group system:masters \
-    --no-duplicate-arns \
-    --username admin-user1
-aws eks update-kubeconfig --region ca-central-1 --name ai-flash-cards-cluster          
+# Create an EKS cluster
+eksctl create cluster --name ai-flash-cards --region ca-central-1 --nodegroup-name node-group --node-type t3.small --nodes 1 --nodes-min 1 --nodes-max 1 --managed
+
+# Create config maps
+kubectl apply -f ../kubernetes/eks/configmap/backend-config.yaml     
+
+# Create secrets
+kubectl apply -f ../kubernetes/eks/secrets/api-secret.yaml  
+
+# Create deployments
+kubectl apply -f ../kubernetes/eks/deploy/backend.yaml    
+kubectl apply -f ../kubernetes/eks/deploy/frontend.yaml  
+
+# Create services
+kubectl apply -f ../kubernetes/eks/svc/backend.yaml    
+kubectl apply -f ../kubernetes/eks/svc/frontend.yaml  
+
+# Create nginx ingress
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.8.2/deploy/static/provider/aws/deploy.yaml
+kubectl apply -f ../kubernetes/eks/ing/main-ingress.yaml   

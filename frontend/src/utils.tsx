626a2d76aed6f9c0575eb3e18b03e28e3aad2389
CheriseCodes <cherise.bry@gmail.com>
2024-02-14 20:01:16 -0500

    trigger actions

--- frontend/src/utils.tsx
+++ frontend/src/utils.tsx
@@ -5,7 +5,6 @@ import { v4 as uuidv4 } from "uuid";
 // TODO: Stop updating card data object directly... only update with update-card reducer
 const PORT = process.env.VITE_BACKEND_PORT;
 const BACKEND_DOMAIN = process.env.VITE_BACKEND_HOST;
-const SECURE_TRANSPORT = (PORT == "443") ? "s" : ""; 
 
 const getNewCardText = async (word: string, languageMode: string, languageLevel: string, userId: string, cardId: string, timeStamp: number) => {
 

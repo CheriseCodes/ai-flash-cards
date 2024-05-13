9af109c9f92539902961b7d4b15c09cb7f09f15a
CheriseCodes <cherise.bry@gmail.com>
2024-05-12 23:45:01 -0400

    match current API

--- frontend/src/mocks/handlers.js
+++ frontend/src/mocks/handlers.js
@@ -1,35 +1,39 @@
 import { HttpResponse, http } from "msw"
 
+import { serviceConfig } from "../config";
+
+const backendDomain = `${serviceConfig.BACKEND_ENDPOINT}${serviceConfig.BACKEND_PATH}`
+
 export const handlers = [
-    http.post("/flashcards",() => {
+    http.get(`${backendDomain}/flashcards`,() => {
         return HttpResponse.json({"data": [{"Content":{"S":"{\"word\": \"blanche\",\"sampleSentence\": \"Ma mère a préparé une sauce blanche pour accompagner les pâtes.\",\"translatedSampleSentence\":\"My mother made a white sauce to accompany the pasta.\",\"wordTranslated\": \"white\"}"},"TimeStamp":{"S":"1701047496686"},"FlashCardId":{"S":"f552286a-4780-4a36-8500-326d3ce6785f"}}]})
     }),
-    http.post("/openai/test/text", ({request}) => {
+    http.get(`${backendDomain}/generations/sentences`, ({request}) => {
         const url = new URL(request.url)
         const word = url.searchParams.get("word")
         const langMode = url.searchParams.get("lang_mode")
         const langLevel = url.searchParams.get("lang_level")
-        return new HttpResponse.json({id: 123, created: 123, usage: {
-            prompt_tokens: 123,
-            completion_tokens: 123,
-            total_tokens: 123
-          }, choices:[{ finish_reason: "stop", message: {content: JSON.stringify({word: `${word}`,sampleSentence: `Example sentence using ${word} in ${langMode} at level ${langLevel}`,translatedSampleSentence:"English translation of the example sentence",wordTranslated: `English translation of ${word}`})}}]})
+        const data = {id: 123, created: 123, usage: {
+                      prompt_tokens: 123,
+                   completion_tokens: 123,
+                        total_tokens: 123
+                     }, choices:[{ finish_reason: "stop", message: {content: JSON.stringify({word: `${word}`,sampleSentence: `Example sentence using ${word} in ${langMode} at level ${langLevel}`,translatedSampleSentence:"English translation of the example sentence",wordTranslated: `English translation of ${word}`})}}]}
+        return HttpResponse.json(data)
     }),
-    http.post("/openai/test/imagine", () => {
+    http.get(`${backendDomain}/generations/images`, () => {
         return HttpResponse.json({
             created: Date.now(),
             data: [
               {
-                url: "https://images.crunchbase.com/image/upload/c_lpad,h_256,w_256,f_auto,q_auto:eco,dpr_1/gavygdwhilk8d2cytkeq"
+                url: "https://picsum.photos/250"
               }
             ]
         })
     }),
-    http.post("/upload/image",({request}) => {
+    http.post(`${backendDomain}/upload/image`,({request}) => {
         const url = new URL(request.url)
-        const imgUrl = url.searchParams.get("imgUrl")
         return HttpResponse.json(
-            { url: imgUrl }
+            { url: "https://picsum.photos/250" }
         )
     })
 ]

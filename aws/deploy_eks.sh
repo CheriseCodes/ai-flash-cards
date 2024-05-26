9f8e3f0a0f21c7b83c0cf9d563f65db845f49e0c
CheriseCodes <cherise.bry@gmail.com>
2024-05-26 14:56:05 -0400

    modify eks config to latest LBC

--- aws/deploy_eks.sh
+++ aws/deploy_eks.sh
@@ -1,5 +1,6 @@
 # Create an EKS cluster
 cluster_name=ai-flash-cards
+AWS_ACCOUNT_ID=$1
 # Min instance size is t3.medium
 eksctl create cluster --name $cluster_name --region ca-central-1 --nodegroup-name node-group --node-type t3.medium --nodes 1 --nodes-min 1 --nodes-max 1 --managed
 
@@ -21,7 +22,7 @@ kubectl apply -f ../kubernetes/eks/svc/frontend.yaml
 # Install AWS Load Balancer Controller add-on
 # create new IAM OIDC provider
 eksctl utils associate-iam-oidc-provider --cluster $cluster_name --approve 
-curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.5.4/docs/install/iam_policy.json # download policy template for controller
+curl -O https://raw.githubusercontent.com/kubernetes-sigs/aws-load-balancer-controller/v2.7.2/docs/install/iam_policy.json # download policy template for controller
 # create the policy using downloaded template
 aws iam create-policy \
     --policy-name AWSLoadBalancerControllerIAMPolicy \
@@ -32,27 +33,28 @@ eksctl create iamserviceaccount \
   --namespace=kube-system \
   --name=aws-load-balancer-controller \
   --role-name AmazonEKSLoadBalancerControllerRole \
-  --attach-policy-arn=arn:aws:iam::$1:policy/AWSLoadBalancerControllerIAMPolicy \
+  --attach-policy-arn=arn:aws:iam::$AWS_ACCOUNT_ID:policy/AWSLoadBalancerControllerIAMPolicy \
   --approve 
 # install cert manager
 kubectl apply \
     --validate=false \
-    -f https://github.com/jetstack/cert-manager/releases/download/v1.13.3/cert-manager.yaml
+    -f https://github.com/jetstack/cert-manager/releases/download/v1.13.5/cert-manager.yaml
 # wait for cert manager resources to be available
 sleep 120
 # download controller spec
-curl -Lo v2_5_4_full.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.5.4/v2_5_4_full.yaml 
-# modify the controller spec if downloaded v2_5_4
-sed -i.bak -e '596,604d' ./v2_5_4_full.yaml
+curl -Lo v2_7_2_full.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.7.2/v2_7_2_full.yaml 
+# modify the controller spec if downloaded v2_7_2
+sed -i.bak -e '596,604d' ./v2_7_2_full.yaml
+sed -i.bak -e "s|your-cluster-name|$cluster_name|" ./v2_7_2_full.yaml
 # (optional) use privately uploaded aws-load-balancer-controller image
-# sed -i.bak -e 's|public.ecr.aws/eks/aws-load-balancer-controller|$AWS_ACCOUNT_ID.dkr.ecr.region-code.amazonaws.com/eks/aws-load-balancer-controller|' ./v2_5_4_full.yaml
+sed -i.bak -e 's|public.ecr.aws/eks/aws-load-balancer-controller|$AWS_ACCOUNT_ID.dkr.ecr.region-code.amazonaws.com/eks/aws-load-balancer-controller|' ./v2_7_2_full.yaml
 
 # install load balancer controller
-kubectl apply -f v2_5_4_full.yaml
+kubectl apply -f v2_7_2_full.yaml
 
 # install load balancer controller ingress class
-curl -Lo v2_5_4_ingclass.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.5.4/v2_5_4_ingclass.yaml
-kubectl apply -f v2_5_4_ingclass.yaml
+curl -Lo v2_7_2_ingclass.yaml https://github.com/kubernetes-sigs/aws-load-balancer-controller/releases/download/v2.7.2/v2_7_2_ingclass.yaml
+kubectl apply -f v2_7_2_ingclass.yaml
 
 # Script that automates this fix: https://github.com/kubernetes-sigs/aws-load-balancer-controller/issues/2289#issuecomment-1953389964k
 kubectl get svc aws-load-balancer-webhook-service -n kube-system -o yaml > aws-load-balancer-webhook-service.yaml
@@ -75,4 +77,4 @@ kubectl apply -f ../kubernetes/eks/ing/main-ingress.yaml
 #   --default-actions Type=forward,TargetGroupArn="arn:aws:elasticloadbalancing:$AWS_REGION:$AWS_ACCOUNT_ID:targetgroup/k8s-default-frontend"
 #   --ssl-policy ELBSecurityPolicy-TLS13-1-2-2021-06
 
-rm iam_policy.json v2_5_4_full.yaml v2_5_4_full.yaml.bak v2_5_4_ingclass.yaml aws-load-balancer-webhook-service.yaml aws-load-balancer-webhook-service.yaml.bak
+rm iam_policy.json v2_7_2_full.yaml v2_7_2_full.yaml.bak v2_7_2_ingclass.yaml aws-load-balancer-webhook-service.yaml aws-load-balancer-webhook-service.yaml.bak

68cf52ac7b40a808bbcaacae95c50625980d6585
CheriseCodes <cherise.bry@gmail.com>
2024-06-21 17:21:11 -0400

    fix deletion

--- helm/delete.sh
+++ helm/delete.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 # Uninstall apps before deleting namespaces to prevent namespace deletion getting stuck
 helm uninstall -n monitoring kube-prometheus-stack
-helm uninstall -n ai-flash-cards
+helm uninstall -n ai-flash-cards ai-flash-cards
 helm uninstall -n ingress-nginx ingress-nginx
 helm plugin uninstall diff
+
+# Delete resources created outside of helm
+kubectl delete -n ai-flash-card secret nginx-ingress-cert
 
 # Delete namespaces
 kubectl delete ns monitoring 

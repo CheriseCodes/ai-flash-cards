37aa0a632b71b01127f25304cf58508f89a43182
CheriseCodes <cherise.bry@gmail.com>
2024-08-22 21:53:33 -0400

    validate outside of handler

--- backend/src/controllers/flashcards-controller.ts
+++ backend/src/controllers/flashcards-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import { dynamoDb, s3 } from "../classes/aws";
 import { dynamoDbClient, s3Client } from "../clients/aws";
+import { validateDeleteFlashcardRequest, validateGetFlashcardsByUserRequest } from "../helpers/validators";
 import { handleDeleteFlashcard, handleGetFlashcardsByUser } from '../handlers/flashcards-handlers';
 import { GenericServerResponse } from '../../types/global';
 
@@ -8,12 +9,25 @@ const s3ClientInstance: s3 = new s3(s3Client);
 const dynamoDbClientInstance: dynamoDb = new dynamoDb(dynamoDbClient);
  
 export const getFlashcardsByUser =  async (req: Request, res: Response) => {
-  const response: GenericServerResponse = await handleGetFlashcardsByUser(req, dynamoDbClientInstance);
+  // check for undefined values
+  const reqError: GenericServerResponse = validateGetFlashcardsByUserRequest(req)
+  if (reqError) {
+    res.status(reqError.status).send(reqError.body);
+  }
+  const userId: string = req.query.userId[0];
+  const response: GenericServerResponse = await handleGetFlashcardsByUser({userId: userId}, dynamoDbClientInstance);
   res.status(response.status).send(response.body);
 }
 
 export const deleteFlashcard =  async (req: Request, res: Response) => {
-    const response: GenericServerResponse = await handleDeleteFlashcard(req, dynamoDbClientInstance, s3ClientInstance);
+    // check for undefined values
+    const reqError: GenericServerResponse = validateDeleteFlashcardRequest(req)
+    if (reqError) {
+      res.status(reqError.status).send(reqError.body);
+    }
+    const userId = req.body.userId;
+    const cardId = req.body.cardId;
+    const response: GenericServerResponse = await handleDeleteFlashcard({ userId: userId, cardId: cardId }, dynamoDbClientInstance, s3ClientInstance);
     res.status(response.status).send(response.body);
 }
 
--- backend/src/handlers/flashcards-handlers.ts
+++ backend/src/handlers/flashcards-handlers.ts
@@ -1,22 +1,17 @@
-import { Request, Response } from 'express';
-import { validateDeleteFlashcardRequest, validateGetFlashcardsByUserRequest } from '../helpers/validators';
 import { dynamoDb, s3 } from '../classes/aws';
 import { GenericServerResponse } from '../../types/global';
 
-export const handleGetFlashcardsByUser =  async (req: Request, dynamoDbClient: dynamoDb): Promise<GenericServerResponse> => {
+interface handleGetFlashcardsByUserInput {
+    userId: string;
+}
+export const handleGetFlashcardsByUser =  async (input: handleGetFlashcardsByUserInput, dynamoDbClient: dynamoDb): Promise<GenericServerResponse> => {
     try {
-        // check for undefined values
-        const reqError: GenericServerResponse = validateGetFlashcardsByUserRequest(req)
-        if (reqError) {
-            return reqError
-        }
-        const userId: string = req.query.userId[0];
         const awsResponse = await dynamoDbClient.query({
           TableName: "FlashCardGenAITable",
           IndexName: "UserId",
           Select: "SPECIFIC_ATTRIBUTES",
           KeyConditionExpression: "UserId = :u",
-          ExpressionAttributeValues: { ":u": { S: userId } },
+          ExpressionAttributeValues: { ":u": { S: input.userId } },
           ProjectionExpression: "#T, FlashCardId, Content, ImageLink",
           ExpressionAttributeNames: { "#T": "TimeStamp" },
         });
@@ -27,27 +22,23 @@ export const handleGetFlashcardsByUser =  async (req: Request, dynamoDbClient: d
       }  
 }
 
-
-export const handleDeleteFlashcard =  async (req: Request, dynamoDbClient: dynamoDb, s3Client: s3): Promise<GenericServerResponse> => {
+interface handleDeleteFlashcardInput {
+    userId: string;
+    cardId: string;
+}
+export const handleDeleteFlashcard =  async (input: handleDeleteFlashcardInput, dynamoDbClient: dynamoDb, s3Client: s3): Promise<GenericServerResponse> => {
     try {
-        // check for undefined values
-        const reqError: GenericServerResponse = validateDeleteFlashcardRequest(req)
-        if (reqError) {
-            return reqError
-        }
-        const userId = req.body.userId;
-        const cardId = req.body.cardId;
         const response = await dynamoDbClient.getItem({ // GetItemInput
           TableName: "FlashCardGenAITable", // required
           Key: { // Key // required
             FlashCardId: { // AttributeValue Union: only one key present
-              S: cardId,
+              S: input.cardId,
             },
           },
           ConsistentRead: true,
         });
-        if (userId != response.Item.UserId.S) {
-          return {status: 400, body: {message: `${userId} is unauthorzed to delete flashcard`}}
+        if (input.userId != response.Item.UserId.S) {
+          return {status: 400, body: {message: `${input.userId} is unauthorzed to delete flashcard`}}
         }
 
         // TODO: Check error before returning response
@@ -61,10 +52,10 @@ export const handleDeleteFlashcard =  async (req: Request, dynamoDbClient: dynam
           await dynamoDbClient.deleteItem({
             TableName: "FlashCardGenAITable",
             Key: {
-              FlashCardId: {S : cardId},
+              FlashCardId: {S : input.cardId},
             },
           });
-          return {status: 200, body: {cardId: cardId}};
+          return {status: 200, body: {cardId: input.cardId}};
         } catch (err) {
           console.error(err);
           return {status: 500, body: {message: "Failed to delete flashcard"}}

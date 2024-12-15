d979f3a28306dee6a2fe7e86e2ecf12485f39bc1
Cherise Bryan <51966982+CheriseCodes@users.noreply.github.com>
2024-12-14 22:15:34 -0500

    re-run actions

--- frontend/__test__/ErrorBanner.test.tsx
+++ frontend/__test__/ErrorBanner.test.tsx
@@ -6,7 +6,7 @@ import { renderWithProviders } from './test-utils';
 import ErrorBanner from '../src/components/ErrorBanner';
 import { it, expect } from '@jest/globals';
 
-it("should have the correct values for a valid error", () => {
+it('should have the correct values for a valid error', () => {
     const eMsg = "This is a new error";
     const { container } = renderWithProviders(<ErrorBanner e={{id:"abc123", message: eMsg}} setErrors={() => {}}></ErrorBanner>);
     const eMsgRes = container.querySelector(".alert-heading + p");
